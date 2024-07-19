dd145566f2eec5c0bcabcb3d4840f837a6a67485
23122vvzxz <144202835+23122vvzxz@users.noreply.github.com>
2024-07-19 14:43:11 +0900

    Update app.py

--- app.py
+++ app.py
@@ -11,8 +11,10 @@ import pickle
 import numpy as np
 import requests
 import io
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 from PIL import Image
-
 
 # utils.py 내용을 직접 포함
 html_temp = """
@@ -51,11 +53,11 @@ dec_temp = """
 """
 
 def main():
-#    col1, col2 = st.columns([3,1])
+    col1, col2 = st.columns([3,1])
 
-#    with col2:
-#        image = Image.open('ui.png')
-#        st.image(image, width=150)
+    with col2:
+        image = Image.open('ui.png')
+        st.image(image, width=150)
 
     st.subheader("부유 진균 농도 예측")
     st.markdown(html_temp, unsafe_allow_html=True)
@@ -68,7 +70,60 @@ def main():
         st.markdown(dec_temp, unsafe_allow_html=True)
     elif choice == 'EDA':
         st.subheader('탐색적 자료 분석(EDA)')
-        # EDA 관련 코드를 여기에 추가하세요
+        
+        @st.cache_data
+        def load_data():
+            data = pd.read_csv('khu.csv')
+            return data
+        
+        data = load_data()
+        #데이터 미리보기
+        st.write("데이터 미리보기:")
+        st.write(data.head())
+
+        #기본 통계량
+        st.write("기본 통계량:")
+        st.write(data.describe())
+
+        #결측치 확인
+        st.write("결측치 확인:")
+        st.write(data.isnull().sum())
+
+        #히스토그램
+        st.write("히스토그램:")
+        fig, ax = plt.subplots()
+        data.hist(bins=15, figsize=(30,20), ax=ax)
+        st.pyplot(fig)
+
+        for ax in fig.axes:
+            ax.tick_params(axis='x', labelsize=1)
+            ax.tick_params(axis='y', labelsize=1)
+            ax.set_title(ax.get_title(), fontsize=2)
+
+        #상관관계 분석
+        st.write("상관관계 히트맵:")
+        fig, ax = plt.subplots()
+        sns.heatmap(data.corr(), annot=False, cmap='coolwarm', ax=ax)
+        st.pyplot(fig)
+
+        #산점도
+        st.write("산점도:")
+        x_axis = st.selectbox('X축 선택', data.columns)
+        y_axis = st.selectbox('Y축 선택', data.columns)
+        color_option = st.selectbox('색상 구분', ['None'] + list(data.columns))
+        fig, ax = plt.subplots(figsize =(10,6))
+
+        if color_option == 'None':
+            sns.scatterplot(data=data, x=x_axis, y=y_axis, ax=ax)
+        else:
+            sns.scatterplot(data=data, x=x_axis, y=y_axis, hue=color_option, ax=ax)
+
+        plt.xlabel(x_axis)
+        plt.ylabel(y_axis)
+        plt.title('plot')
+
+        st.pyplot(fig)
+      
     elif choice == 'ML':
         st.subheader('머신러닝(ML)')
         # ML 관련 코드를 여기에 추가하세요
